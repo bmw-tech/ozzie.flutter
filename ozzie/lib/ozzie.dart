25ed9deb926f3de9056aef438eb5a98433c0577a
Jorge Coca <jorge.coca@bmwna.com>
2018-10-09 10:32:05 -0500

    Now ozzie is able to generate HTML reports with created screenshots

--- ozzie/lib/ozzie.dart
+++ ozzie/lib/ozzie.dart
@@ -4,7 +4,6 @@ import 'dart:async';
 import 'dart:io';
 import 'package:meta/meta.dart';
 import 'package:flutter_driver/flutter_driver.dart';
-import 'html_report.dart';
 import 'reporter.dart';
 
 const _rootFolderName = "ozzie";
@@ -29,10 +28,8 @@ class Ozzie {
   }
 
   Future generateHtmlReport() async {
-    final file = File('$_rootFolderName/index.html');
     final reporter = Reporter();
-    await reporter.getOzzieFiles(_rootFolderName);
-    await file.writeAsString(htmlReport);
+    await reporter.generateHtmlReport(_rootFolderName);
   }
 
   Future _deleteExistingGroupFolder() async {
--- ozzie/lib/reporter.dart
+++ ozzie/lib/reporter.dart
@@ -1,20 +1,92 @@
 import 'dart:async';
+import 'dart:math';
 import 'dart:io';
 
+import 'package:ozzie/html_report.dart';
+
 class Reporter {
-  Future<Map<String, List<String>>> getOzzieFiles(String rootFolderName) async {
+  Future generateHtmlReport(String rootFolderName) async {
+    final ozzieFiles = await _getOzzieFiles(rootFolderName);
+    final imageGallery = _buildImageGallery(ozzieFiles);
+    String htmlContent =
+        '$beginningOfHtmlReport$imageGallery$endingOfHtmlReport';
+    final file = File('$rootFolderName/index.html');
+    await file.writeAsString(htmlContent);
+  }
+
+  Future<Map<String, List<String>>> _getOzzieFiles(
+      String rootFolderName) async {
     final rootDirectory = Directory(rootFolderName);
     final allFiles =
         rootDirectory.listSync(recursive: false, followLinks: false);
     final directories =
         allFiles.where((f) => (f is Directory)).map((f) => f as Directory);
-    var ozzieFiles = Map();
+    var ozzieFiles = Map<String, List<String>>();
     directories.forEach((directory) {
       final screenshots = directory
           .listSync(recursive: false, followLinks: false)
-          .map((s) => s.path);
+          .map((s) => s.path.replaceAll(rootFolderName, ''))
+          .toList();
       ozzieFiles[directory.path] = screenshots;
     });
     return ozzieFiles;
+  }
+
+  String _buildImageGallery(Map<String, List<String>> ozzieFiles) {
+    var accordionBuffer = StringBuffer();
+    ozzieFiles.keys.forEach((String entryName) {
+      final entry = _buildAccordion(entryName, ozzieFiles[entryName]);
+      accordionBuffer.write(entry);
+    });
+    final accordion = accordionBuffer.toString();
+
+    return """
+<div class="accordion" id="ozzieAccordion" style="width: 100%;">
+  $accordion
+</div>  
+    """;
+  }
+
+  String _buildAccordion(String accordionName, List<String> images) {
+    final randomId = accordionName.length.toString();
+    return """
+<div class="card">
+  <div class="card-header" id="heading$randomId">
+    <h5 class="mb-0">
+      <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapse$randomId" aria-expanded="true" aria-controls="collapse$randomId">
+        $accordionName
+      </button>
+    </h5>
+  </div>
+  <div id="collapse$randomId" class="collapse" aria-labelledby="heading$randomId" data-parent="#ozzieAccordion">
+    <div class="card-body">
+      ${_buildImages(images)}
+    </div>
+  </div>
+</div>
+    """;
+  }
+
+  String _buildImages(List<String> images) {
+    var imageCardsBuffer = StringBuffer();
+    images.forEach((imagePath) {
+      imageCardsBuffer.write("""
+<div class="col-md-4">
+  <div class="card mb-4 shadow-sm">
+    <img class="card-img-top" style="width: 100%; display: block;" src="./$imagePath" data-holder-rendered="true">
+  </div>
+</div>
+      """);
+    });
+    final imageCards = imageCardsBuffer.toString();
+    return '<div class="row">$imageCards</div>';
+  }
+
+  String _generateRandomId(int length) {
+    var rand = new Random();
+    var codeUnits = new List.generate(length, (index) {
+      return rand.nextInt(33) + 89;
+    });
+    return new String.fromCharCodes(codeUnits);
   }
 }
