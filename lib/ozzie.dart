9854313e432fe70b21bf8bac767371b69ec2e677
Thiago Augusto Gonçalves Fernandes Santos Silva <wstagf@gmail.com>
2020-04-29 19:08:05 -0300

    Replace without 'new Reg'

--- lib/ozzie.dart
+++ lib/ozzie.dart
@@ -124,7 +124,7 @@ class Ozzie {
   String get _groupFolderName => '$rootFolderName/$groupName';
 
   String get _timestamp =>
-      DateTime.now().toIso8601String().replaceAll(new RegExp(r':'), '.');
+      DateTime.now().toIso8601String().replaceAll(':', '.');
 
   String _fileName(String screenshotName) => '$_timestamp-$screenshotName.png';
 
